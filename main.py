6d8154ed19602a917c0d758c79bd09e027edff3c
lukedev45 <lukedevlin45@gmail.com>
2026-01-12 20:17:33 +0000

    Creating groups

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    py_clock = pygame.time.Clock()
+    dt = 0
+
+    ## Creating game groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    ## Main game loop
     while True:
         log_state()
+        screen.fill("black")
+        for object in drawable:
+            object.draw(screen)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill("black")
         pygame.display.flip()
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = py_clock.tick(60) / 1000
+        updatable.update(dt)
 
 
 if __name__ == "__main__":
