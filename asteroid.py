5b003bcb04f4cf056e527ef0601944c3a7179fcd
lukedev45 <lukedevlin45@gmail.com>
2026-01-14 21:04:15 +0000

    Added asteroids to the game

--- asteroid.py
+++ asteroid.py
@@ -6,8 +6,8 @@ class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
-    def draw(self, screen, color, pos, radius, line_width=LINE_WIDTH):
-        pygame.draw.circle(screen, color, pos, radius, line_width)
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
     
     def update(self, dt):
         self.position += self.velocity * dt 
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -12,8 +14,12 @@ def main():
     ## Creating game groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     ## Main game loop
     while True:
