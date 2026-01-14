6109c6caa8befb95800b79fd19cc5d066c973521
lukedev45 <lukedevlin45@gmail.com>
2026-01-14 21:18:15 +0000

    Shots now actually collide and I added a rate limiter

--- player.py
+++ player.py
@@ -1,11 +1,12 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS, SHOT_RADIUS
 import pygame
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         self.rotation = 180
+        self.shot_cooldown = 0
         super().__init__(x, y, PLAYER_RADIUS)
 
     def triangle(self):
@@ -25,6 +26,8 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        
+        self.shot_cooldown -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -44,6 +47,9 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
+        if self.shot_cooldown > 0:
+            return
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         velocity = pygame.Vector2(0, 1)
         velocity = velocity.rotate(self.rotation)
